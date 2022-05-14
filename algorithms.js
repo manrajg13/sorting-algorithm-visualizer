10b035f5ccaa55680c0d1d55862128e0e484939f
Manraj <manraj836@gmail.com>
2022-05-13 20:38:33 -0400

    Update algorithms.js

--- algorithms.js
+++ algorithms.js
@@ -142,6 +142,7 @@ async function insertionSort(chart, array) {
             }
             else{
                 j--;
+                await colorBar(chart, j-1, mscColor, 1);
             }
         }
         colorBar(chart, i, dftColor, 0);
